59b8bbc7be59a50fb101edfd3e02c0da6efa7eda
Sabaresh <Sabaresh.kumar@Dvara.com>
2023-12-02 14:41:35 +0530

    db connection

--- api/app.js
+++ api/app.js
@@ -1,8 +1,15 @@
 
 import express, { json, urlencoded } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
 
-
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('Connected to MongoDb');
+}).catch((err)=>{
+  console.log(err);
+})
 
 var app = express();
 
